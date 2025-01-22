33b51d2a8a37d3db73867e8df06c70efe11806f4
John Cedric Panti <inhog.cedrick@gmail.com>
2025-01-22 17:11:22 +0800

    Add navigation
    * Implemented navigation functionality using TanStack Router

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,9 +11,23 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as ProductsImport } from './routes/products'
+import { Route as AboutImport } from './routes/about'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
+
+const ProductsRoute = ProductsImport.update({
+  id: '/products',
+  path: '/products',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const AboutRoute = AboutImport.update({
+  id: '/about',
+  path: '/about',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   id: '/',
@@ -32,6 +46,20 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/about': {
+      id: '/about'
+      path: '/about'
+      fullPath: '/about'
+      preLoaderRoute: typeof AboutImport
+      parentRoute: typeof rootRoute
+    }
+    '/products': {
+      id: '/products'
+      path: '/products'
+      fullPath: '/products'
+      preLoaderRoute: typeof ProductsImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -39,32 +67,42 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/about': typeof AboutRoute
+  '/products': typeof ProductsRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/about': typeof AboutRoute
+  '/products': typeof ProductsRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/about': typeof AboutRoute
+  '/products': typeof ProductsRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/about' | '/products'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/about' | '/products'
+  id: '__root__' | '/' | '/about' | '/products'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  AboutRoute: typeof AboutRoute
+  ProductsRoute: typeof ProductsRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  AboutRoute: AboutRoute,
+  ProductsRoute: ProductsRoute,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +115,19 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/about",
+        "/products"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/about": {
+      "filePath": "about.tsx"
+    },
+    "/products": {
+      "filePath": "products.tsx"
     }
   }
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-import * as React from "react";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { ShoppingCart, UserRound } from "lucide-react";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -7,8 +7,29 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <React.Fragment>
+    <div className="container mx-auto my-9 max-w-screen-xl">
+      <header className="flex items-center justify-between py-6">
+        <p className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+          <span>Tan</span>
+          <span className="text-green-500">Cart</span>
+        </p>
+        <nav className="flex items-center gap-5 text-sm font-medium uppercase">
+          <Link to="/" className="[&.active]:text-green-500/90">
+            Home
+          </Link>
+          <Link to="/products" className="[&.active]:text-green-500/90">
+            Products
+          </Link>
+          <Link to="/about" className="[&.active]:text-green-500/90">
+            About
+          </Link>
+        </nav>
+        <div className="flex items-center gap-3">
+          <UserRound />
+          <ShoppingCart />
+        </div>
+      </header>
       <Outlet />
-    </React.Fragment>
+    </div>
   );
 }
